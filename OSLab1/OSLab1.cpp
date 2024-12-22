5c74dea5f7949f86dc0b6f93904eaa0690c107e8
Anigilator322 <Anigilator322@gmail.com>
2024-12-22 17:07:09 +0700

    update 1lab

--- OSLab1/OSLab1.cpp
+++ OSLab1/OSLab1.cpp
@@ -2,98 +2,102 @@
 #include <thread>
 #include <queue>
 #include <mutex>
-#include <condition_variable>
-#include <chrono>
 #include <random>
-#include <atomic>
 
 using namespace std;
 
-struct Event {
-    int _data;
-    std::thread::id _Sender;
-    Event(int data, std::thread::id sender)
-    {
-        _data = data;
-        _Sender = sender;
-    }
+struct Message
+{
+	int _data;
+	thread::id _SenderId;
+	Message(int data, thread::id id)
+	{
+		_data = data;
+		_SenderId = id;
+	};
 };
-
-class Monitor {
-public:
-    void addEvent(const Event& event) {
-        unique_lock<mutex> lock(_mutex);
-        _queue.push(event);
-        _cv.notify_one();
-    }
-
-    bool getEvent(Event& event) {
-        unique_lock<mutex> lock(_mutex);
-        _cv.wait(lock, [this] { return !_queue.empty() || _isStopped; });
-        if (_isStopped && _queue.empty()) {
-            return false;
-        }
-        event = _queue.front();
-        _queue.pop();
-        return true;
-    }
-
-    void stop() {
-        unique_lock<mutex> lock(_mutex);
-        _isStopped = true;
-        _cv.notify_all();
-    }
-
+class Monitor
+{
 private:
-    queue<Event> _queue;
-    mutex _mutex;
-    condition_variable _cv;
-    bool _isStopped = false;
+	queue<Message> _queue;
+	mutex _mutex;
+	condition_variable _condition;
+	bool _isStopped = false;
+public:
+	void SendMessage(const Message& message)
+	{
+		unique_lock<mutex> lock(_mutex);
+		_queue.push(message);
+		_condition.notify_all();
+	}
+
+	bool TryReceiveMessage(Message& message)
+	{
+		unique_lock<mutex> lock(_mutex);
+		_condition.wait(lock, [this] {return !_queue.empty() || _isStopped; });
+		if (_isStopped && _queue.empty())
+		{
+			return false;
+		}
+		message = _queue.front();
+		_queue.pop();
+		return true;
+	}
+
+	void Stop()
+	{
+		unique_lock<mutex> lock(_mutex);
+		_isStopped = true;
+		_condition.notify_all();
+	}
 };
 
-void supplier(Monitor& monitor) {
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> dist(1, 100);
+void Consumer(Monitor& monitor)
+{
+	while (true) {
+		Message message(0, thread::id());
+		if (!monitor.TryReceiveMessage(message)) {
+			break;
+		}
 
-    for (int i = 0; i < 5; ++i) {
-        Event event = Event(dist(gen), this_thread::get_id());
+		cout << "Потребитель: Получено событие с данными " << message._data << " Отправитель: " << message._SenderId << endl;
+		cout << "Потребитель: Обработка события..." << endl;
+		this_thread::sleep_for(chrono::milliseconds(1000));
+	}
 
-        cout << "Поставщик " << event._Sender << ": Сгенерировано событие с данными " << event._data << endl;
-        monitor.addEvent(event);
-
-        this_thread::sleep_for(chrono::milliseconds(500));
-    }
-
-    cout << "Поставщик: Завершение работы." << endl;
+	cout << "Потребитель: Завершение работы." << endl;
 }
 
-void consumer(Monitor& monitor) {
-    while (true) {
-        Event event(0, thread::id());
-        if (!monitor.getEvent(event)) {
-            break;
-        }
+void Supplier(Monitor& monitor)
+{
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<> dist(1, 100);
 
-        cout << "Потребитель: Получено событие с данными " << event._data << " Отправитель: " << event._Sender << endl;
-        cout << "Потребитель: Обработка события..." << endl;
-        this_thread::sleep_for(chrono::milliseconds(1000));
-    }
+	for (int i = 0; i < 5; ++i) {
+		Message message = Message(dist(gen), this_thread::get_id());
 
-    cout << "Потребитель: Завершение работы." << endl;
+		cout << "Поставщик " << message._SenderId << ": Сгенерировано событие с данными " << message._data << endl;
+		monitor.SendMessage(message);
+
+		this_thread::sleep_for(chrono::milliseconds(500));
+	}
+
+	cout << "Поставщик: Завершение работы." << endl;
 }
 
-int main() {
-    Monitor monitor;
-    setlocale(LC_ALL, "russian");
 
-    thread supplierThread(supplier, ref(monitor));
-    thread consumerThread(consumer, ref(monitor));
+int main() 
+{
+	setlocale(LC_ALL, "russian");
+	Monitor monitor;
+	thread supplierThread(Supplier, ref(monitor));
+	thread consumerThread(Consumer, ref(monitor));
 
-    supplierThread.join();
-    monitor.stop();
-    consumerThread.join();
+	supplierThread.join();
+	monitor.Stop();
+	consumerThread.join();
 
-    return 0;
+	return 0;
 }
 
